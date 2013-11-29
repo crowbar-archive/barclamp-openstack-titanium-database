c3a77c253639b0f1edb5d81688c995694d81ff14
Keith Rochford <keith.rochford@gmail.com>
2013-11-29 10:04:08 +0000

    added preseed section before package

--- percona/chef/cookbooks/percona/recipes/cluster.rb
+++ percona/chef/cookbooks/percona/recipes/cluster.rb
@@ -3,6 +3,27 @@
 # install packages
 case node["platform_family"]
 when "debian"
+directory "/var/cache/local/preseeding" do
+    owner "root"
+    group "root"
+    mode 0755
+    recursive true
+  end
+
+  execute "preseed percona-server" do
+    command "debconf-set-selections /var/cache/local/preseeding/percona-server.seed"
+    action :nothing
+  end
+
+  template "/var/cache/local/preseeding/percona-server.seed" do
+    source "percona-server.seed.erb"
+    owner "root"
+    group "root"
+    mode "0600"
+    notifies :run, resources(:execute => "preseed percona-server"), :immediately
+  end
+
+
   package "percona-xtradb-cluster-server-5.5" do
     options "--force-yes"
   end
