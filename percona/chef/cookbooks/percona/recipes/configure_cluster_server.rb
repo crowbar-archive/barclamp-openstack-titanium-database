cdc2f1ba0354ff3e15683fe924146856e7843ec3
Keith Rochford <keith.rochford@gmail.com>
2013-12-09 19:10:22 +0000

    fixed (3) platform check in root password block

--- percona/chef/cookbooks/percona/recipes/configure_cluster_server.rb
+++ percona/chef/cookbooks/percona/recipes/configure_cluster_server.rb
@@ -136,10 +136,9 @@ end
 
 # now let's set the root password only if this is the initial install
 execute "Update MySQL root password" do
-  #if node["platform_family"] != "debian"   #on debian this should have already been taken care of with debconf-set-selections
   command "mysqladmin --user=root --password='' password '"+root_password+"'"
-  #end
-  not_if "test -f /tmp/percona_grants.sql" || platform_family?("debian")
+  only_if { node["platform_family"] != "debian" }  #on debian this should have already been taken care of with debconf-set-selections
+  not_if "test -f /tmp/percona_grants.sql" #|| platform_family?("debian")
   #not_if node["platform_family"] == "debian"
   #not_if platform_family?("debian")
 end
