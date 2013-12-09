a78d94912ef1122ad44bda911121bd050029e4b5
Keith Rochford <keith.rochford@gmail.com>
2013-12-09 19:29:18 +0000

    added removemyconf ruby block

--- percona/chef/cookbooks/percona/recipes/configure_cluster_server.rb
+++ percona/chef/cookbooks/percona/recipes/configure_cluster_server.rb
@@ -21,6 +21,18 @@ gcommaddr = "gcomm://" +  cont1_admin_ip + "," + cont2_admin_ip + "," + cont3_ad
 root_password = node["percona"]["server_root_password"]
 debian_password = node["percona"]["server_debian_password"]
 
+# check if an server config file is in place and if so, remove it (might replace this with a ruby block to rename
+ruby_block "removemyconf" do
+  block do
+    require 'date'
+    #if File.ctime(percona["main_config_file"]).to_date < Date.today
+    if File.exist?("/etc/mysql/my.cnf") && Date.parse(File.ctime("/etc/mysql/my.cnf").to_s) < Date.today
+                Chef::Log.info("****COE-LOG: Deleting pre-existing my.cnf config file")
+                File.delete(percona["main_config_file"])
+        end
+  end
+  action :create
+end
 
 #template "/root/.my.cnf" do
 #  variables(:root_password => root_password)
