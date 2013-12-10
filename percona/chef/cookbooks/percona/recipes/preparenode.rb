bd6ac6186b85f853c93f5471c3a1ce5f0b6a60b8
Keith Rochford <keith.rochford@gmail.com>
2013-12-10 11:49:58 +0000

    removed date check on file

--- percona/chef/cookbooks/percona/recipes/preparenode.rb
+++ percona/chef/cookbooks/percona/recipes/preparenode.rb
@@ -4,7 +4,8 @@ ruby_block "removemyconf" do
   block do
     require 'date'
     #if File.ctime(percona["main_config_file"]).to_date < Date.today
-    if File.exist?("/etc/mysql/my.cnf") && Date.parse(File.ctime("/etc/mysql/my.cnf").to_s) < Date.today
+    #if File.exist?("/etc/mysql/my.cnf") && Date.parse(File.ctime("/etc/mysql/my.cnf").to_s) < Date.today
+    if File.exist?(percona["main_config_file"]) 
                 Chef::Log.info("****COE-LOG: Deleting pre-existing my.cnf config file")
                 File.delete(percona["main_config_file"])
         end
